31c98e1319145a86cecc846b6d4b4378f10896c7
JoseRossLess <valarmorghul373@gmail.com>
2024-07-15 22:34:20 -0600

    Starting

--- BaseCalculator.py
+++ BaseCalculator.py
@@ -1,1 +1,8 @@
-""" Here we  Start to work, pleas enter YES"""
+
+# Para la parte visual de la calculadora se escibe el siguiente codigo 
+# Se implementa la extensión Tkinter Snnipets que es una biblioteca gráfica para Python
+from tkinter import *
+from tkinter import ttk
+import math
+
+root = Tk()
